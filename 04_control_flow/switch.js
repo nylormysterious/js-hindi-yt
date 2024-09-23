2b6533b6b381261d2b1ebe49be86f42554a5fce8
Tanisha Gupta <89590199+nylormysterious@users.noreply.github.com>
2024-09-23 12:58:34 +0000

    switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        //shift+alt +down arrow isse duplicate hota
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
